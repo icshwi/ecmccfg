2dddecd1019670fc2e03f5a1506d38a04dc7b1c7
Julen Etxeberria <julen.etxeberriamalkorra@ess.eu>
2020-04-03 11:12:09 +0200

    icBLM IOC configuration ready

--- hardware/ESS_crates/ecmcicBLM.cmd
+++ hardware/ESS_crates/ecmcicBLM.cmd
@@ -1,12 +1,12 @@
 ############################################################
-############# icBLM
+############# icBLM HVPS ISEG EtherCAT HW
 
 
 #0  0:0  PREOP  +  EK1100 EtherCAT-Koppler (2A E-Bus)
 #1  0:1  PREOP  +  EL3174-0002 4K. Ana. Eingang  +/-10V, +/-20mA, 16 Bit, Galvanis
 #2  0:2  PREOP  +  EL4134 4K. Ana. Ausgang -10/+10V. 16bit
 #3  0:3  PREOP  +  EL9505 Netzteilklemme 5V
-#4  0:4  PREOP  +  EL1252-0050 2K. Fast Dig. Eingang 5V, 1�s, DC Latch
+#4  0:4  PREOP  +  EL1252-0050 2K. Fast Dig. Eingang 5V, 1ï¿½s, DC Latch
 #5  0:5  PREOP  +  EL2124 4K. Dig. Ausgang 5V, 20mA
 
 #Configure EK1100 coupler terminal
@@ -21,7 +21,7 @@ ${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=2, HW_DESC=EL4134"
 # Configure EL9505 power supply 5V
 ${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=3, HW_DESC=EL9505"
 
-# Configure EL2124 digital output terminal 5V.
+# Configure EL1252 digital input terminal 5V.
 ${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=4, HW_DESC=EL1252"
 
 # Configure EL2124 digital output terminal 5V.
